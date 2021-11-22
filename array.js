bd1274e8e3406e608e25851b609c2f40cbe627e2
Anastasia Panoutsou <panoutsou.anastasia@gmail.com>
2021-11-22 14:09:38 +0000

    add image and array

---
+++ array.js
@@ -0,0 +1,6 @@
+let shoppingList = [];
+let food = ["bananas", "tuna"];
+let drinks = ["coke", "sprite", "coffee"];
+let cleaning = ["soap", "bleach"];
+shoppingList = [food, drinks, cleaning];
+console.log(shoppingList[1][2]);
